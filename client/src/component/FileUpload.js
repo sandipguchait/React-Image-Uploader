b3b7b0e5dd1310c7a24cfa24131767d4de28a7c0
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-11 00:54:58 +0530

    finishing FileUpload component

--- client/src/component/FileUpload.js
+++ client/src/component/FileUpload.js
@@ -1,11 +1,15 @@
 import React, { Fragment, useState } from 'react';
+import Message from './Message';
+import Progress from './Progress';
 import axios from 'axios';
 
 const FileUpload = () => {
 
     const [ file, setFile] = useState('');
-    const [ filename, setFilename] = useState('Choose File');
-    const [uploadedFile, setUploadedFile] = useState({});
+    const [ filename, setFilename ] = useState('Choose File');
+    const [ uploadedFile, setUploadedFile ] = useState({});
+    const [ message, setMessage ] = useState('');
+    const [ uploadPercentage, setUploadPercentage ] = useState(0);
 
     const onChange = (e) => {
         setFile(e.target.files[0]);
@@ -22,23 +26,35 @@ const FileUpload = () => {
             const response = await axios.post('/upload', formData, {
                 headers: {
                     "Content-Type": 'multipart/form-data'
-                }
+                },
+                onUploadProgress: progressEvent => {
+                    setUploadPercentage(parseInt(Math.round(( progressEvent.loaded * 100 ) /
+                    progressEvent.total
+                    )))
+
+                    // clear percentage
+                    setTimeout(() => setUploadPercentage(0), 10000);
+                },
             });
             // When file gets uploaded success, we get a JSON data from backend as filename, filepath
-            const { filename, filepath } = response.data;
+            const { fileName, filePath } = response.data;
             //setting its value to state
-            setUploadedFile({ filename, filepath });
+            setUploadedFile({ fileName, filePath });
+
+            setMessage('File Uploaded')
+
         } catch(err) {
             if( err.response.status === 500 ) {
-                console.log('problem with server');
+                setMessage('problem with server');
             } else {
-                console.log(err.response.data.message)
+                setMessage(err.response.data.message)
             }
         }
     };
 
     return (
         <Fragment>
+            { message && <Message message={message}/> }
             <form onSubmit={onSubmit}>
                 <div className="custom-file mb-4">
                     <input type="file" className="custom-file-input" id="customFile" onChange={onChange}/>
@@ -46,8 +62,15 @@ const FileUpload = () => {
                         {filename}
                     </label>
                 </div>
+                <Progress percentage={uploadPercentage} />
                 <input type="submit" value="Upload" className="btn btn-primary btn-block mt-4"/>
             </form>
+            { uploadedFile ? (<div className="row mt-5">
+                <div className="col-md-6 m-auto">
+                    <h3 className="text-center">{ uploadedFile.fileName }</h3>
+                    <img style={{ width: '100%'}} src={uploadedFile.filePath} alt=""/>
+                </div>
+            </div>) : null }
         </Fragment>
     );
 };
