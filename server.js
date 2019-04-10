45923b073b62dc6972cda731f12a079286d07682
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-10 23:18:46 +0530

    adding client folder

--- server.js
+++ server.js
@@ -1,3 +1,33 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 
+const app = express();
+
+app.use(fileUpload());
+
+
+
+//Upload endpoint 
+app.post('/upload', (req, res) => {
+    
+    if( req.files === null ) {
+        return res.status(400).json({ message: 'No file Uploaded' });
+    }
+
+    const file = req.files.file;
+
+    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+        if(err) {
+            console.error(err);
+            return res.status(500).send(err);
+        }
+        //if file successfully uploaded
+        res.json({ fileName: file.name, filePath: `/uploads/${file.name}`});
+    });
+});
+
+
+
+app.listen(5000, () => {
+    console.log('SERVER STARTED')
+});
