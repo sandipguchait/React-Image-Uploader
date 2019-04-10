cf8a533721fc9b0c76f4229ec5cddd404af55f5e
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-10 23:45:42 +0530

    using hooks to setup the ui and the file upload system

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import FileUpload from './component/FileUpload';
 
   const App = () => (
-      <div className="App">
-        hello
+      <div className="container mt-4">
+        <h4 className="display-4 text-center mb-4">
+          <i className="fab fa-react"></i> React file Upload
+        </h4>
+        <FileUpload/>
       </div>
     );
 
---
+++ client/src/component/FileUpload.js
@@ -0,0 +1,33 @@
+import React, { Fragment, useState } from 'react';
+
+const FileUpload = () => {
+
+    const [ file, setFile] = useState('');
+    const [ filename, setFilename] = useState('Choose File');
+
+    const onChange = (e) => {
+        setFile(e.target.files[0]);
+        setFilename(e.target.files[0].name);
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+        
+    }
+
+    return (
+        <Fragment>
+            <form onSubmit={onSubmit}>
+                <div className="custom-file mb-4">
+                    <input type="file" className="custom-file-input" id="customFile" onChange={onChange}/>
+                    <label className="custom-file-label" htmlFor="customFile">
+                        {filename}
+                    </label>
+                </div>
+                <input type="submit" value="Upload" className="btn btn-primary btn-block mt-4"/>
+            </form>
+        </Fragment>
+    );
+};
+
+export default FileUpload;
