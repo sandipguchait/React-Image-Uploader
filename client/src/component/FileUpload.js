71ff9026ba69f7c9f6b98132e76181c007df3245
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-04-11 00:05:16 +0530

    file uploaded succesfully

--- client/src/component/FileUpload.js
+++ client/src/component/FileUpload.js
@@ -1,19 +1,41 @@
 import React, { Fragment, useState } from 'react';
+import axios from 'axios';
 
 const FileUpload = () => {
 
     const [ file, setFile] = useState('');
     const [ filename, setFilename] = useState('Choose File');
+    const [uploadedFile, setUploadedFile] = useState({});
 
     const onChange = (e) => {
         setFile(e.target.files[0]);
         setFilename(e.target.files[0].name);
     }
 
-    const onSubmit = e => {
+    const onSubmit = async e => {
         e.preventDefault();
-        
-    }
+        const formData = new FormData();
+        formData.append('file', file);
+
+        try{
+            //posting the file to the server
+            const response = await axios.post('/upload', formData, {
+                headers: {
+                    "Content-Type": 'multipart/form-data'
+                }
+            });
+            // When file gets uploaded success, we get a JSON data from backend as filename, filepath
+            const { filename, filepath } = response.data;
+            //setting its value to state
+            setUploadedFile({ filename, filepath });
+        } catch(err) {
+            if( err.response.status === 500 ) {
+                console.log('problem with server');
+            } else {
+                console.log(err.response.data.message)
+            }
+        }
+    };
 
     return (
         <Fragment>
